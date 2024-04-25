c7b85148ffe9bafa6a054944095035008e0244a0
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-04-25 18:47:43 +0530

    Add files via upload

---
+++ Constructor/Constru.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Constru
+    {
+        public int id;
+        public String name;
+        public Constru(int id)
+        {
+          //  this(2, "hfggh");
+            this.id = id;
+        }
+        public Constru(int id, string name )
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}

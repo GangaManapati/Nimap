193173157e7384dad902e75d02b532b42339e486
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-04-30 15:57:24 +0530

    Add files via upload

---
+++ Lambda/eventsDeligatesLinq/eventsDeligatesLinq/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eventsDeligatesLinq
+{
+
+    /* internal class Program
+     {
+         public delegate float D2(int a, float b);
+         public delegate void D1(String s1, string s2);
+
+
+         public float Method1(int a, float b)
+         {
+            // Console.WriteLine("the combination of s, and s1 is:" + a + b);
+             return a + b;
+         }
+         public void Method2(String s1, String s2)
+         {
+             Console.WriteLine("the combination of s, and s1 is:" + s1 +"***"+ s2);
+         }
+         static void Main(string[] args)
+         {
+             Program x=new Program();
+            //Program o=new Program();
+            // y = y + x.Method2;// the two mwthods which having the same params and return type.
+            D2 z=x.Method1;
+             D1 y = x.Method2;
+
+             var r=z.Invoke(10,34.6f);
+             y.Invoke("ganga", "bhavani");
+             Console.WriteLine(r);
+
+             Console.ReadLine();
+         }
+     }   
+
+
+ }
+         */
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+

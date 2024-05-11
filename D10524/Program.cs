2e55259ad2f61e3757c0414197af3e4ba2e552a8
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-05-11 17:25:47 +0530

    Add files via upload

---
+++ D10524/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D10524
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new FirstDATABEntities();
+            var Product = new List<Product>
+            {
+                new Product
+                {
+                    ProductID = 3,
+                    Name = "gaa",
+                    Description = "grnong",
+                    Price = 364.5M,
+                    StockQuantity = 45
+                },
+                new Product
+                {
+                    ProductID = 4,
+                    Name = "river",
+                    Description = "afternoon",
+                    Price = 7646.2M,
+                    StockQuantity = 20
+                },
+                new Product
+                {
+                    ProductID = 5,
+                    Name = "ravi",
+                    Description = "afrnoon",
+                    Price = 6.2M,
+                    StockQuantity = 33
+                }
+
+            };
+            c.Products.AddRange(Product);
+            c.SaveChanges();
+            Console.ReadLine();
+        }
+    }
+}

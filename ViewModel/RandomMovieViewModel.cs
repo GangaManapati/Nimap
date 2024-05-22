d7b8fc3b4220ff822571c8404b9745b03beb9d6f
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-05-22 18:14:24 +0530

    Add files via upload

---
+++ ViewModel/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Day22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day22.ViewModel
+{
+    public class RandomMovieViewModel
+    {
+        public Movie M { get; set; }    
+        public List<Customer> Cus { get; set; }
+    }
+}
